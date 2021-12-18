009b6ebe829447f8df3329afa1537b0ebf03fbb2
hama <hmmshl@gmail.com>
2021-12-18 20:47:48 +0100

    devise installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_131530) do
+ActiveRecord::Schema.define(version: 2021_12_18_191633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,9 @@ ActiveRecord::Schema.define(version: 2021_05_20_131530) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.string "categories"
-    t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "tags"
   end
 
 end
