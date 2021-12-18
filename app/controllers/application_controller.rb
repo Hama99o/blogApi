31048a85aa455f3b2b9bd799d873389477c640e1
hama <hmmshl@gmail.com>
2021-12-18 21:15:30 +0100

    routes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::API
   def redirect_to_quotes
     redirect_to api_v1_articles_path
   end
+
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "application#redirect_to_quotes"
-  
-  namespace :api do
+
+  namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       resources :articles
+      resources :users, only: %w[show]
     end
   end
+
+ devise_for :users,
+   defaults: { format: :json },
+   path: '',
+   path_names: {
+     sign_in: 'api/v1/login',
+     sign_out: 'api/v1/logout',
+     registration: 'api/v1/signup'
+   },
+   controllers: {
+     sessions: 'sessions',
+     registrations: 'registrations'
+   }
 end
