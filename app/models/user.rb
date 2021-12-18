41eed89a8f43d66956b0e963993369a8d9f5e588
hama <hmmshl@gmail.com>
2021-12-18 22:58:17 +0100

    PASSWORD_FORMAT added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+  /x
+
+  validates :password,
+    presence: true,
+    length: { in: Devise.password_length },
+    format: { with: PASSWORD_FORMAT },
+    confirmation: true,
+    on: :create
+
+  validates :password,
+    allow_nil: true,
+    length: { in: Devise.password_length },
+    format: { with: PASSWORD_FORMAT },
+    confirmation: true,
+    on: :update
+
   devise :database_authenticatable, :registerable,
           :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
